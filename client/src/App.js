112e178ae1a07c0301e1af6b5e325b970935ced7
vladzyaba <vladzyaba@gmail.com>
2021-03-26 16:54:59 +0300

    client: added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+import {
+  Home,
+  Login,
+  Register,
+} from './pages';
+
+const PATHS = {
+  HOME:     '/',
+  LOGIN:    '/login',
+  REGISTER: '/register',
+};
+
 function App() {
   return (
-    <>
-      <h1>React post</h1>
-    </>
+    <BrowserRouter>
+      <div>
+        <Link to={PATHS.HOME}>Home</Link>
+        <Link to={PATHS.LOGIN}>login</Link>
+        <Link to={PATHS.REGISTER}>register</Link>
+      </div>
+      <Switch>
+        <Route exact path={PATHS.HOME} component={Home} />
+        <Route exact path={PATHS.LOGIN} component={Login} />
+        <Route exact path={PATHS.REGISTER} component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
