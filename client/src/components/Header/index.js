9bef661fdad08682833030d8ee43fbfc31b4f40a
vladzyaba <vladzyaba@gmail.com>
2021-03-26 18:11:28 +0300

    client: header refactoring

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,58 @@
-import { NavLink } from 'react-router-dom';
+import React                 from 'react';
+import classNames            from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
 
 import './scss/style.scss';
 
 import PATHS from '../../paths';
 
-export default function Header() {
+function Header() {
+  const {pathname}          = useLocation();
+  const path                = pathname === '/' ? 'home' : pathname.slice(1);
+  const [active, setActive] = React.useState();
+
+  React.useEffect(() => {
+    setActive(path);
+  }, [pathname, path]);
+
+  const handleClick = ({target: {name}}) => {
+    setActive(name);
+  };
+
+  console.log('render');
+
   return (
     <header className={'header'}>
       <nav className={'navigation'}>
         <ul>
-          <li><NavLink to={PATHS.HOME}>Home</NavLink></li>
-          <li><NavLink to={PATHS.LOGIN}>login</NavLink></li>
-          <li><NavLink to={PATHS.REGISTER}>register</NavLink></li>
+          <li>
+            <Link
+              className={classNames({'active': active === 'home'})}
+              to={PATHS.HOME}
+              onClick={handleClick}
+              name={'home'}
+            >Home</Link>
+          </li>
+          <li>
+            <Link
+              className={classNames({'active': active === 'login'})}
+              to={PATHS.LOGIN}
+              onClick={handleClick}
+              name={'login'}
+            >login</Link>
+          </li>
+          <li>
+            <Link
+              className={classNames({'active': active === 'register'})}
+              to={PATHS.REGISTER}
+              onClick={handleClick}
+              name={'register'}
+            >register</Link>
+          </li>
         </ul>
       </nav>
     </header>
   );
 }
+
+export default React.memo(Header);
